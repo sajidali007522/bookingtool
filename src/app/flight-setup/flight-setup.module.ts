4a844901c3478085066e920ac7d70657b8ed0bea
Basit Munir <basitmunir.pk@gmail.com>
2020-10-12 10:55:34 +0500

    component integration

--- src/app/flight-setup/flight-setup.module.ts
+++ src/app/flight-setup/flight-setup.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FlightsComponent } from './components/flights/flights.component';
+import {routing} from "./flight-setup.routing";
 
 
 
 @NgModule({
   declarations: [FlightsComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    routing
   ]
 })
 export class FlightSetupModule { }
