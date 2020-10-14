c467b336f87360ee3ec75339ef929318a39d7dd5
Basit Munir <basitmunir.pk@gmail.com>
2020-10-14 23:55:49 +0500

    setup completed

--- src/app/demo-housekeeping/demo-housekeeping.module.ts
+++ src/app/demo-housekeeping/demo-housekeeping.module.ts
@@ -1,15 +1,47 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HousekeepingComponent } from './components/housekeeping/housekeeping.component';
+import {
+  BrowserModule,
+  HAMMER_GESTURE_CONFIG,
+  HammerGestureConfig,
+  HammerModule,
+} from '@angular/platform-browser';
+import * as Hammer from 'hammerjs';
+
 import {routing} from "./demo-housekeeping.routing";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+
+import { HousekeepingComponent } from './components/housekeeping/housekeeping.component';
+import { RoomImageComponent } from './components/room-image/room-image.component';
+import {RoomComponent} from "./components/room/room.component";
+import {ImageCropperModule} from "ngx-image-cropper";
+import {ConfirmModalComponent} from "../shared/confirm-modal/confirm-modal.component";
+import {AutocompleteLibModule} from "angular-ng-autocomplete";
+import {SharedModuleModule} from "../shared-module/shared-module.module";
 
 
-
+export class MyHammerConfig extends HammerGestureConfig {
+  overrides = <any>{
+    swipe: { direction: Hammer.DIRECTION_ALL },
+    pinch: { enable: false },
+    rotate: { enable: false },
+  };
+}
 @NgModule({
-  declarations: [HousekeepingComponent],
+  declarations: [
+    HousekeepingComponent,
+    RoomImageComponent,
+    RoomComponent,
+    ConfirmModalComponent
+  ],
   imports: [
     CommonModule,
-    routing
+    routing,
+    FormsModule,
+    ReactiveFormsModule,
+    ImageCropperModule,
+    AutocompleteLibModule,
+    SharedModuleModule
   ]
 })
 export class DemoHousekeepingModule { }
