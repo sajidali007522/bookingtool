e97a03367bb5127ab48a5173aac6af5e31df3d33
Basit Munir <basitmunir.pk@gmail.com>
2020-10-21 23:07:00 +0500

    missing service fix

---
+++ src/app/_services/demo-housekeeping.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {AuthService} from "../auth.service";
+import {ConfigService} from "../config.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DemoHousekeepingService {
+
+  apiBaseUrl = 'https://demo.innfinity.com/productsdemo/api3/'
+  constructor(private http: HttpClient, private _auth:AuthService, private appConfigService: ConfigService) {}
+
+  public loadRooms(url, params={}) {
+    let headers = new HttpHeaders().set(this._auth.getAuthKey(),  this._auth.getToken());
+    return this.http.get(this.appConfigService.apiBaseUrl+url, {
+      params: params,
+      headers: headers
+    });
+    //  .pipe(shareReplay({ bufferSize: 1, refCount: true }));
+  }
+
+  public loadRoom(url, params={}) {
+    let headers = new HttpHeaders().set(this._auth.getAuthKey(),  this._auth.getToken());
+    return this.http.get(this.appConfigService.apiBaseUrl+url, {
+      params: params,
+      headers: headers
+    });
+    //  .pipe(shareReplay({ bufferSize: 1, refCount: true }));
+  }
+}
