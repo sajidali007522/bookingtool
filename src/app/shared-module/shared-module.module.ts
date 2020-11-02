e2e857cc15f14a322575a121e0826de82413df36
Basit <basitmunir.pk@gmail.com>
2020-11-02 13:42:22 -0800

    check

--- src/app/shared-module/shared-module.module.ts
+++ src/app/shared-module/shared-module.module.ts
@@ -2,17 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {LoaderComponent} from "./components/loader/loader.component";
 import {ModalComponent} from "./components/modal/modal.component";
+import { ConfirmModalComponent } from '../shared/confirm-modal/confirm-modal.component';
 
 
 
 @NgModule({
-  declarations: [LoaderComponent, ModalComponent],
+  declarations: [LoaderComponent, ModalComponent, ConfirmModalComponent],
   imports: [
     CommonModule
   ],
   exports: [
     LoaderComponent,
-    ModalComponent
+    ModalComponent,
+    ConfirmModalComponent
   ]
 })
 export class SharedModuleModule { }
