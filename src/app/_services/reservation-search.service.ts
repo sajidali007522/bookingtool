5b03e03e5a7cefd1aba60acceda3503adad62859
M Basit Munir <basitmunir.pk@gmail.com>
2021-04-08 17:18:42 +0500

    missing service

---
+++ src/app/_services/reservation-search.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import {HttpService} from "../http.service";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {AuthService} from "../auth.service";
+import {DateParser} from "../_helpers/dateParser";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ReservationSearchService {
+  baseUrl = 'https://demo.innfinity.com/productsdemo/api3/'
+  state = {
+    errorMessages: []
+  }
+  constructor(
+    private _http: HttpService,
+    private http: HttpClient,
+    private _auth:AuthService,
+    private dateParse: DateParser
+  ) { }
+
+  public loadCriteriaDefinition () {
+    return this.http.get( `${this.baseUrl}ReservationSearch/CriteriaDefinition`);
+  }
+
+  public loadResultDefinition () {
+    return this.http.get( `${this.baseUrl}ReservationSearch/ResultDefinition`);
+  }
+
+  public makeSearch (params) {
+    return this.http.get( `${this.baseUrl}ReservationSearch/Search`, params);
+  }
+
+}
