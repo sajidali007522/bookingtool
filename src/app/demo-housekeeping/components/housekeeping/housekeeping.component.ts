ba8a214f036963d2f480f1cd61b90aebbd3099c0
M Basit Munir <basitmunir.pk@gmail.com>
2021-05-21 19:48:59 +0500

    updates

--- src/app/demo-housekeeping/components/housekeeping/housekeeping.component.ts
+++ src/app/demo-housekeeping/components/housekeeping/housekeeping.component.ts
@@ -220,6 +220,7 @@ export class HousekeepingComponent implements OnInit, AfterViewInit, AfterViewCh
     this.state.pagination.pageNum=1;
     this.state.loadMetaData = true
     this.metaDataGroups=[];
+    this.state.massEdit.items = []
     this.loadRooms();
   }
 
