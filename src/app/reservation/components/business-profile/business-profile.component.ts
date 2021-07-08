7023a604ce1ef8c192f41880af7e9b417415e1cf
M Basit Munir <basitmunir.pk@gmail.com>
2021-07-08 22:51:59 +0500

    updates

--- src/app/reservation/components/business-profile/business-profile.component.ts
+++ src/app/reservation/components/business-profile/business-profile.component.ts
@@ -154,6 +154,11 @@ export class BusinessProfileComponent implements OnInit,AfterViewInit, AfterView
     })
       .subscribe(res=>{
         if(res['status'] == 200) {
+          if(this.resources.length>0) {
+            for (let index = 0; index < this.resources.length; index++) {
+              this.resources[index]['isOpen'] = false;
+            }
+          }
           this.resources.push(res['data']);
           //this.state.selectedTemplate['resources'].push(res['data']['resources'])
           this.setResourcesDate((this.resources.length-1));
@@ -163,8 +168,10 @@ export class BusinessProfileComponent implements OnInit,AfterViewInit, AfterView
         err=>{
           this.state.loadingTemplate = false;
         },
-        ()=>{this.state.loadingTemplate = false}
-        )
+        ()=>{this.state.loadingTemplate = false
+          this.form.resourceTypeID = '00000000-0000-0000-0000-000000000000'
+        }
+      )
   }
   setProfile(){
     //​/api2​/booking​/{bookingID}​/Reporting
